16a332be994628251a0071290304afbf57e555ed
ziadjbr <ziad.jabir@mail.mcgill.ca>
2023-10-22 13:14:21 -0400

    Created Character object file

---
+++ FitnessApp/src/Components/Character.tsx
@@ -0,0 +1,10 @@
+type Character = {
+    id: number;
+    name: string;
+    idleAnimation: string[];
+    walkingAnimation: string[];
+    workingOutAnimation: string[];
+  }
+
+  export default Character;
+
